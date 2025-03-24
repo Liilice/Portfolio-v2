73ed5db369dbd2438cb9c3a0c4bdf0a30a62296f
Alice Zheng <lilice@MacBook-Pro-de-Alice.local>
2025-03-24 22:53:19 +0100

    FIX (portfolio) : responsive 'project'

--- Portfolio/src/components/Contact.tsx
+++ Portfolio/src/components/Contact.tsx
@@ -41,107 +41,112 @@ const Contact = () => {
   };
 
   return (
-    <div
-      className="flex flex-col sm:flex-row sm:justify-between items-center"
-      id="contact"
-    >
-      <div className="sm:mr-7 text-white text-[16px] flex sm:flex-col cursor-pointer">
-        <div className="flex items-center py-5">
-          <img
-            src={phone}
-            alt="phone"
-            className="mr-3"
-            onClick={() => handleInfo("phone")}
-          />
-          <p className="hidden sm:block">{contact.phone}</p>
+    <>
+      <h2 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl tracking-widest mt-4">
+        Contact
+      </h2>
+      <div
+        className="flex flex-col sm:flex-row sm:justify-between items-center"
+        id="contact"
+      >
+        <div className="sm:mr-7 text-white text-[16px] flex sm:flex-col cursor-pointer">
+          <div className="flex items-center py-5">
+            <img
+              src={phone}
+              alt="phone"
+              className="mr-3"
+              onClick={() => handleInfo("phone")}
+            />
+            <p className="hidden sm:block">{contact.phone}</p>
+          </div>
+          <div className="flex items-center  py-5">
+            <img
+              src={mail}
+              alt="mail"
+              className="mr-3"
+              onClick={() => handleInfo("mail")}
+            />
+            <p className="hidden sm:block">{contact.mail}</p>
+          </div>
+          <div className="flex items-center  py-5">
+            <img
+              src={location}
+              alt="location"
+              className="mr-3"
+              onClick={() => handleInfo("location")}
+            />
+            <p className="hidden sm:block">{contact.location}</p>
+          </div>
+          <div className="flex justify-center items-center sm:py-5 ">
+            <a
+              href="https://github.com/Liilice"
+              target="_blank"
+              className="max-w-[30%] m-3"
+            >
+              <img src={github} alt="github" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/alicezhenglilice/"
+              target="_blank"
+              className="max-w-[30%] m-3"
+            >
+              <img src={linkedin} alt="linkedin" />
+            </a>
+          </div>
         </div>
-        <div className="flex items-center  py-5">
-          <img
-            src={mail}
-            alt="mail"
-            className="mr-3"
-            onClick={() => handleInfo("mail")}
-          />
-          <p className="hidden sm:block">{contact.mail}</p>
-        </div>
-        <div className="flex items-center  py-5">
-          <img
-            src={location}
-            alt="location"
-            className="mr-3"
-            onClick={() => handleInfo("location")}
-          />
-          <p className="hidden sm:block">{contact.location}</p>
-        </div>
-        <div className="flex justify-center items-center sm:py-5 ">
-          <a
-            href="https://github.com/Liilice"
-            target="_blank"
-            className="max-w-[30%] m-3"
+        {contactInfo && <p className="contactInfo sm:hidden">{contactInfo}</p>}
+        <form onSubmit={submit} className="flex flex-col sm:pl-2 flex-1 w-full">
+          <label
+            className="fontSolid tracking-[.3em] text-white "
+            htmlFor="email"
           >
-            <img src={github} alt="github" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/alicezhenglilice/"
-            target="_blank"
-            className="max-w-[30%] m-3"
+            Email
+          </label>
+          <input
+            className={`rounded-xl px-4 py-3 mb-4  bg-white/75 shadow-2xl ${
+              emailValided === false
+                ? "border-4 border-dashed border-red-800"
+                : ""
+            }`}
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label
+            htmlFor="object"
+            className="fontSolid tracking-[.3em] text-white"
           >
-            <img src={linkedin} alt="linkedin" />
-          </a>
-        </div>
-      </div>
-      {contactInfo && <p className="contactInfo sm:hidden">{contactInfo}</p>}
-      <form onSubmit={submit} className="flex flex-col sm:pl-2 flex-1 w-full">
-        <label
-          className="fontSolid tracking-[.3em] text-white "
-          htmlFor="email"
-        >
-          Email
-        </label>
-        <input
-          className={`rounded-xl px-4 py-3 mb-4  bg-white/75 shadow-2xl ${
-            emailValided === false
-              ? "border-4 border-dashed border-red-800"
-              : ""
-          }`}
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label
-          htmlFor="object"
-          className="fontSolid tracking-[.3em] text-white"
-        >
-          Objet
-        </label>
-        <input
-          type="text"
-          className="w-[100%] rounded-xl  px-4 py-3 mb-4  bg-white/75 shadow-2xl"
-          id="object"
-          name="object"
-          value={object}
-          onChange={(e) => setObject(e.target.value)}
-        />
-        <label
-          htmlFor="message"
-          className="fontSolid tracking-[.3em] text-white"
-        >
-          Message
-        </label>
-        <textarea
-          name="message"
-          className="w-[100%] rounded-xl  px-4 py-3 mb-6  bg-white/75 shadow-2xl"
-          id="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          required
-        ></textarea>
+            Objet
+          </label>
+          <input
+            type="text"
+            className="w-[100%] rounded-xl  px-4 py-3 mb-4  bg-white/75 shadow-2xl"
+            id="object"
+            name="object"
+            value={object}
+            onChange={(e) => setObject(e.target.value)}
+          />
+          <label
+            htmlFor="message"
+            className="fontSolid tracking-[.3em] text-white"
+          >
+            Message
+          </label>
+          <textarea
+            name="message"
+            className="w-[100%] rounded-xl  px-4 py-3 mb-6  bg-white/75 shadow-2xl"
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          ></textarea>
 
-        <Button text={"Envoyer"} />
-      </form>
-    </div>
+          <Button text={"Envoyer"} />
+        </form>
+      </div>
+    </>
   );
 };
 
